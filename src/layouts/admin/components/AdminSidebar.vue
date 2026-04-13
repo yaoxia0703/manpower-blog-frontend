5e32add54faaa4d7d440fd5ec2d1eb91910093f8
Yao Xia <yx18627792049@gmail.com>
2026-04-13 22:39:36 +0900

    feat(#8):サイドバー側のメニューを動的にレンダリングする処理を実装

--- src/layouts/admin/components/AdminSidebar.vue
+++ src/layouts/admin/components/AdminSidebar.vue
@@ -1,31 +1,41 @@
 <template>
   <el-menu
-    default-active="/admin/dashboard"
+    :default-active="route.path"
     class="menu"
     background-color="#001529"
     text-color="#fff"
     active-text-color="#409eff"
     router
   >
+    <!-- 固定首页 -->
     <el-menu-item index="/admin/dashboard">
-      仪表盘
+      ダッシュボード
     </el-menu-item>
 
-    <el-menu-item index="/admin/user">
-      用户管理
-    </el-menu-item>
+    <!-- 动态菜单 -->
+    <template v-for="menu in menus" :key="menu.id">
+      <MenuItem :menu="menu" />
+    </template>
 
-    <el-menu-item index="/admin/role">
-      角色管理
-    </el-menu-item>
-
-    <el-menu-item index="/admin/menu">
-      菜单管理
-    </el-menu-item>
   </el-menu>
 </template>
 
 <script setup lang="ts">
+defineOptions({
+  name: 'AdminSidebar'
+})
+import { usePermissionStore } from '@/stores/permissionStore'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import MenuItem from './MenuItem.vue'
+
+const permissionStore = usePermissionStore()
+const route = useRoute()
+
+// 过滤掉按钮类型（type = 2）
+const menus = computed(() =>
+  permissionStore.menus.filter(menu => menu.type !== 2)
+)
 </script>
 
 <style scoped>
